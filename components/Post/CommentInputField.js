481b344205aa764a321e0ad73815b116a2f88410
rons9n <rons9n@gmail.com>
2022-05-24 18:52:37 -0400

    NoData component

---
+++ components/Post/CommentInputField.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const CommentInputField = () => {
+    return (
+        <div>
+            CommentInputField
+        </div>
+    )
+}
+
+export default CommentInputField
--- pages/index.js
+++ pages/index.js
@@ -1,17 +1,33 @@
 import React, { useEffect } from 'react'
 import axios from 'axios'
 import baseUrl from '../utils/baseUrl'
+import CreatePost from '../components/Post/CreatePost'
+import CardPost from '../components/Post/CardPost'
+import { Segment } from 'semantic-ui-react'
+import { parseCookies } from 'nookies'
+import { NoPosts } from '../components/Layout/NoData'
 
-function Index({ user, userFollowStats }) {
+function Index({ user }) {
+
     useEffect(() => {
         document.title = `Welcome, ${user.name.split(" ")[0]}`
     }, [])
 
-    return (
-        <div>
-            Homepage
-        </div>
-    )
+    return <div>Homepage</div>
+}
+
+Index.getInitialProps = async ctx => {
+    try {
+        const { token } = parseCookies(ctx)
+
+        const res = await axios.get(`${baseUrl}/api/posts`, {
+            headers: { Authorization: token }
+        })
+
+        return { postData: res.data }
+    } catch (error) {
+        return { errorLoading: true }
+    }
 }
 
 export default Index
