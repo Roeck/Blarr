668fb07d96484e71e66dc3c7dd0fa16eb310db70
rons9n <rons9n@gmail.com>
2022-05-24 17:43:09 -0400

    Like a post API request

--- api/posts.js
+++ api/posts.js
@@ -98,4 +98,39 @@ router.delete("/:postId", authMiddleware, async (req, res) => {
     }
 });
 
+// LIKE A POST
+
+router.post("/like/:postId", authMiddleware, async (req, res) => {
+    try {
+        const { postId } = req.params;
+        const { userId } = req;
+
+        const post = await PostModel.findById(postId);
+        if (!post) {
+            return res.status(404).send("No Post found");
+        }
+
+        const isLiked =
+            post.likes.filter(like => like.user.toString() === userId).length > 0;
+
+        if (isLiked) {
+            return res.status(401).send("Post already liked");
+        }
+
+        await post.likes.unshift({ user: userId });
+        await post.save();
+
+        const postByUserId = post.user.toString();
+
+        if (postByUserId !== userId) {
+            await newLikeNotification(userId, postId, postByUserId);
+        }
+
+        return res.status(200).send("Post liked");
+    } catch (error) {
+        console.error(error);
+        return res.status(500).send(`Server error`);
+    }
+});
+
 module.exports = router
