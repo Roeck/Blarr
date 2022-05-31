df4092645037710fd3be44c45418f06ed0c4f5b1
rons9n <rons9n@gmail.com>
2022-05-31 19:41:50 -0400

    Chat component

--- components/Layout/Layout.js
+++ components/Layout/Layout.js
@@ -14,10 +14,13 @@ import {
   GridColumn,
 } from "semantic-ui-react";
 import nprogress from "nprogress"
-import Router from "next/router"
+import Router, { useRouter } from "next/router"
 
 function Layout({ children, user }) {
   const contextRef = createRef()
+  const router = useRouter()
+
+  const messagesRoute = router.pathname === "/messages"
 
   Router.onRouteChangeStart = () => nprogress.start();
   Router.onRouteChangeComplete = () => nprogress.done();
@@ -31,37 +34,45 @@ function Layout({ children, user }) {
           <div style={{ marginLeft: "1rem", marginRight: "1rem" }}>
             <Ref innerRef={contextRef}>
               <Grid>
-                <Grid.Column floated="left" width={2}>
-                  <Sticky context={contextRef}>
-                    <SideMenu user={user} />
-                  </Sticky>
-                </Grid.Column>
+                {!messagesRoute ? (
+                  <>
+                    <Grid.Column floated="left" width={2}>
+                      <Sticky context={contextRef}>
+                        <SideMenu user={user} />
+                      </Sticky>
+                    </Grid.Column>
 
-                <Grid.Column width={10}>
-                  <Visibility context={contextRef}>{children}</Visibility>
-                </Grid.Column>
+                    <Grid.Column width={10}>
+                      <Visibility context={contextRef}>{children}</Visibility>
+                    </Grid.Column>
 
-                <Grid.Column floated="left" width={4}>
-                  <Sticky context={contextRef}>
-                    <Segment basic>
-                      <Search />
-                    </Segment>
-                  </Sticky>
-                </Grid.Column>
+                    <Grid.Column floated="left" width={4}>
+                      <Sticky context={contextRef}>
+                        <Segment basic>
+                          <Search />
+                        </Segment>
+                      </Sticky>
+                    </Grid.Column>
+                  </>
+                ) : (
+                  <>
+                    <Grid.Column floated="left" width={1} />
+                    <Grid.Column width={15}>{children}</Grid.Column>
+                  </>
+                )}
               </Grid>
             </Ref>
           </div>
+          ) : (
+          <>
+            <Navbar />
+            <Container style={{ paddingTop: "1rem" }} text>
+              {children}
+            </Container>
+          </>
+          );
         </>
-      ) : (
-        <>
-          <Navbar />
-          <Container style={{ paddingTop: "1rem" }} text>
-            {children}
-          </Container>
-        </>
-      )};
-    </>
-  );
+      );
 }
 
-export default Layout;
+      export default Layout;
