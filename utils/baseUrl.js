999e104b83afd18991f08086e2e5923768b5370f
rons9n <rons9n@gmail.com>
2022-07-02 20:47:51 -0400

    Heroku config, app title update

--- utils/baseUrl.js
+++ utils/baseUrl.js
@@ -1,3 +1,3 @@
-const baseUrl = "http://localhost:3000";
+const baseUrl = process.env.NODE_ENV !== 'production' ? "http://localhost:3000" : "https://blarr.herokuapp.com";
 
 module.exports = baseUrl;
